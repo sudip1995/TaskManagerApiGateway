c4ccb850cf4cc1b391053606d98b2af1fe7e7354
sdpsarker88 <sdpsarker88@gmail.com>
2020-05-16 23:59:22 +0600

    jsonconvert

--- TaskManagerAPIGateway/Controllers/GraphQLController.cs
+++ TaskManagerAPIGateway/Controllers/GraphQLController.cs
@@ -40,10 +40,12 @@ namespace TaskManagerAPIGateway.Controllers
                 Query = query.Query,
             };
 
+           
+
             if (query.Variables != null)
             {
                 var inputs = query.Variables.ToInputs();
-                var jsonInput = "{" + BuildJson(inputs) + "}";
+                var jsonInput = JsonConvert.SerializeObject(inputs);
                 request.Inputs = jsonInput;
             }
             var result = Client.Execute(request);
@@ -58,72 +60,6 @@ namespace TaskManagerAPIGateway.Controllers
             }
             
             return Ok(data);
-        }
-
-        public static string BuildJson(IEnumerable resultData)
-        {
-            var ans = "";
-            foreach (var data in resultData)
-            {
-                var key = data.GetPropertyValue("Key");
-                var value = data.GetPropertyValue("Value");
-
-                if (key == null)
-                {
-                    continue;
-                }
-                //var type = value.GetType();
-
-                if (value is List<object> list)
-                {
-                    ans += JsonConvert.SerializeObject(key);
-                    ans += ": ";
-                    ans += "[";
-                    foreach (var l in list)
-                    {
-                        ans += "{";
-                        ans += BuildJson(l as IEnumerable);
-                        ans += "}";
-                        ans += ",";
-                    }
-                    if (ans.EndsWith(","))
-                    {
-                        ans = ans.Remove(ans.Length - 1);
-                    }
-                    ans += "]";
-                }
-                else if (value is Dictionary<string, object> array)
-                {
-                    ans += JsonConvert.SerializeObject(key);
-                    ans += ": ";
-                    ans += "{";
-                    ans += BuildJson(array);
-                    ans += "}";
-                }
-                else
-                {
-                    ans += BuildData(data);
-                }
-
-                ans += ",";
-            }
-            if (ans.EndsWith(","))
-            {
-                ans = ans.Remove(ans.Length - 1);
-            }
-
-            return ans;
-        }
-
-        private static string BuildData(object data)
-        {
-            var ans = "";
-            var key = data.GetPropertyValue("Key");
-            var value = data.GetPropertyValue("Value");
-            ans += JsonConvert.SerializeObject(key);
-            ans += ": ";
-            ans += JsonConvert.SerializeObject(value);
-            return ans;
         }
     }
 }
