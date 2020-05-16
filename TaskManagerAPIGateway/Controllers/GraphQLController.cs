9b6cb5f0d212ed7a651bc2f6a668b8933caa0350
sdpsarker88 <sdpsarker88@gmail.com>
2020-05-16 20:41:18 +0600

    graphql input works

--- TaskManagerAPIGateway/Controllers/GraphQLController.cs
+++ TaskManagerAPIGateway/Controllers/GraphQLController.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GraphQL;
 using Grpc.Core;
 using Grpc.Net.Client;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using TaskManager.Services;
 using TaskManagerAPIGateway.GraphQL;
 
@@ -32,8 +37,15 @@ namespace TaskManagerAPIGateway.Controllers
         {
             var request = new Request
             {
-                Query = query.Query
+                Query = query.Query,
             };
+
+            if (query.Variables != null)
+            {
+                var inputs = query.Variables.ToInputs();
+                var jsonInput = "{" + BuildJson(inputs) + "}";
+                request.Inputs = jsonInput;
+            }
             var result = Client.Execute(request);
             var data = String.Empty;
             await foreach (var response in result.ResponseStream.ReadAllAsync())
@@ -46,6 +58,72 @@ namespace TaskManagerAPIGateway.Controllers
             }
             
             return Ok(data);
+        }
+
+        public static string BuildJson(IEnumerable resultData)
+        {
+            var ans = "";
+            foreach (var data in resultData)
+            {
+                var key = data.GetPropertyValue("Key");
+                var value = data.GetPropertyValue("Value");
+
+                if (key == null)
+                {
+                    continue;
+                }
+                //var type = value.GetType();
+
+                if (value is List<object> list)
+                {
+                    ans += JsonConvert.SerializeObject(key);
+                    ans += ": ";
+                    ans += "[";
+                    foreach (var l in list)
+                    {
+                        ans += "{";
+                        ans += BuildJson(l as IEnumerable);
+                        ans += "}";
+                        ans += ",";
+                    }
+                    if (ans.EndsWith(","))
+                    {
+                        ans = ans.Remove(ans.Length - 1);
+                    }
+                    ans += "]";
+                }
+                else if (value is Dictionary<string, object> array)
+                {
+                    ans += JsonConvert.SerializeObject(key);
+                    ans += ": ";
+                    ans += "{";
+                    ans += BuildJson(array);
+                    ans += "}";
+                }
+                else
+                {
+                    ans += BuildData(data);
+                }
+
+                ans += ",";
+            }
+            if (ans.EndsWith(","))
+            {
+                ans = ans.Remove(ans.Length - 1);
+            }
+
+            return ans;
+        }
+
+        private static string BuildData(object data)
+        {
+            var ans = "";
+            var key = data.GetPropertyValue("Key");
+            var value = data.GetPropertyValue("Value");
+            ans += JsonConvert.SerializeObject(key);
+            ans += ": ";
+            ans += JsonConvert.SerializeObject(value);
+            return ans;
         }
     }
 }
--- TaskManagerAPIGateway/GraphQL/GraphQLQuery.cs
+++ TaskManagerAPIGateway/GraphQL/GraphQLQuery.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
 
 namespace TaskManagerAPIGateway.GraphQL
 {
     public class GraphQLQuery
     {
+        public string OperationName { get; set; }
+        public string NamedQuery { get; set; }
         public string Query { get; set; }
+        public JObject Variables { get; set; }
     }
 }
